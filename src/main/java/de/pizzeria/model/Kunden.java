250e22069d6ce29a8102e3f679b4cdf0af043bcb
mika.gross <mika.gross@KKSH1OG052PC15.kks.kreis-bergstrasse.net>
2026-02-20 12:39:25 +0100

    Methoden Kopf 20.02.2026

--- src/main/java/de/pizzeria/model/Kunden.java
+++ src/main/java/de/pizzeria/model/Kunden.java
@@ -1,5 +1,18 @@
 package de.pizzeria.model;
 public class Kunden {
-String name;
+    private int kundennumer; 
+    private String name;
+    private String adresse;
+    private String Telefonummer;
+
+    
+    public Kunden(int kundennummer, String name, String adresse, String telefonnummer){
+       this.kundennumer = kundennummer;
+       this.name = name;
+       this.adresse = adresse;
+       this.Telefonummer = telefonnummer;
+    
+    }
 
 }
+
