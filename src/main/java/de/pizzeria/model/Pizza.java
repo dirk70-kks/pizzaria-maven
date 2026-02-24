62e7a1ab93eac58fefd9260ea2eca516c70b960d
artur.schwabauer <artur.schwabauer@KKSH1OG052PC16.kks.kreis-bergstrasse.net>
2026-02-24 10:16:30 +0100

    20.02.2026

--- src/main/java/de/pizzeria/model/Pizza.java
+++ src/main/java/de/pizzeria/model/Pizza.java
@@ -8,6 +8,7 @@ public class Pizza {
     private String groesse;
     private double basisPreis;
     private List<Zutat> zutatenliste;
+    private List<Pizza> pizzaliste;
     
     public Pizza(int pizzanr, string name, string groesse, double basisPreis) {
         
@@ -16,12 +17,67 @@ public class Pizza {
         this.groesse = groesse;
         this.basisPreis = basisPreis;
         this.zutatenliste = newArraylist<>();
+        this.pizzaliste = newArrayList<>();
         
+    }
+    
+    public void addPizza(Pizza pizza) {
+        
+        this.pizzaliste.add(pizza);
     }
     
     public double berechnePreis() {
         
         return basisPreis;
     }
+
+    public int getPizzanr() {
+        return pizzanr;
+    }
+
+    public void setPizzanr(int pizzanr) {
+        this.pizzanr = pizzanr;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGroesse() {
+        return groesse;
+    }
+
+    public void setGroesse(String groesse) {
+        this.groesse = groesse;
+    }
+
+    public double getBasisPreis() {
+        return basisPreis;
+    }
+
+    public void setBasisPreis(double basisPreis) {
+        this.basisPreis = basisPreis;
+    }
+
+    public List<Zutat> getZutatenliste() {
+        return zutatenliste;
+    }
+
+    public void setZutatenliste(List<Zutat> zutatenliste) {
+        this.zutatenliste = zutatenliste;
+    }
+
+    public List<Pizza> getPizzaliste() {
+        return pizzaliste;
+    }
+
+    public void setPizzaliste(List<Pizza> pizzaliste) {
+        this.pizzaliste = pizzaliste;
+    }
 }
+
 
